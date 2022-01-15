6895d0c0f600a4d767fbdfdc8772c76aaa384163
JKGzenna <juanlu360@gmail.com>
2022-01-15 23:41:04 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "allowed_cidr" {
-  type        = "list"
+  type        = list(string)
   default     = ["0.0.0.0/0"]
   description = "A list of CIDR Networks to allow ssh access to."
 }
 
 variable "allowed_ipv6_cidr" {
-  type        = "list"
+  type        = list(string)
   default     = ["::/0"]
   description = "A list of IPv6 CIDR Networks to allow ssh access to."
 }
 
 variable "allowed_security_groups" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "A list of Security Group ID's to allow access to."
 }
